8979d9cb548750cf6d7248baa9efac01cf7be217
omer <gs_omer_ozkan@hotmail.com>
2021-01-21 23:09:20 +0300

    web layer was created and startup settings were made.

--- CvApp.Entities/Concrete/Base/BaseEntityOther.cs
+++ CvApp.Entities/Concrete/Base/BaseEntityOther.cs
@@ -1,7 +1,10 @@
-﻿namespace CvApp.Entities.Concrete.Base
+﻿using Dapper.Contrib.Extensions;
+
+namespace CvApp.Entities.Concrete.Base
 {
     public class BaseEntityOther
     {
+        [Key]
         public int Id { get; set; }
         public string Description { get; set; }
     }
---
+++ CvApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using CvApp.Business.Interfaces;
+using CvApp.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CvApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IGenericService<Skill> skillService;
+        public HomeController(IGenericService<Skill> skillService)
+        {
+            this.skillService = skillService;
+        }
+        public IActionResult Index()
+        {
+            return View(skillService.GetAll());
+        }
+    }
+}
