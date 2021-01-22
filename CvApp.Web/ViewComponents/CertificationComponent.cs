0c206beac223152ec98368e13638c8fb3c4d26fd
omer <gs_omer_ozkan@hotmail.com>
2021-01-22 22:37:49 +0300

    the project is generally completed

---
+++ CvApp.Web/ViewComponents/CertificationComponent.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using CvApp.Business.Interfaces;
+using CvApp.DTO.DTOs.CertificationDtos;
+using CvApp.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CvApp.Web.ViewComponents
+{
+    public class CertificationComponent : ViewComponent
+    {
+        private readonly IGenericService<Certification> _genericCertificationService;
+        private readonly IMapper _mapper;
+
+        public CertificationComponent(IGenericService<Certification> genericCertificationService, IMapper mapper)
+        {
+            _genericCertificationService = genericCertificationService;
+            _mapper = mapper;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_mapper.Map<List<CertificationListDto>>(_genericCertificationService.GetAll()));
+        }
+    }
+}
---
+++ CvApp.Web/ViewComponents/SkillComponent.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CvApp.Business.Interfaces;
+using CvApp.DTO.DTOs.SkillDtos;
+using CvApp.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CvApp.Web.ViewComponents
+{
+    public class SkillComponent : ViewComponent
+    {
+        private readonly IGenericService<Skill> _genericSkillService;
+        private readonly IMapper _mapper;
+        public SkillComponent(IGenericService<Skill> genericSkillService, IMapper mapper)
+        {
+            _genericSkillService = genericSkillService;
+            _mapper = mapper;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_mapper.Map<List<SkillListDto>>(_genericSkillService.GetAll()));
+        }
+    }
+}
